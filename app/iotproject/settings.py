f0604038ff5e83173402350efd3751a8e827c422
sho <69189153+spadr@users.noreply.github.com>
2021-05-28 08:28:49 +0900

    Add files via upload

--- app/iotproject/settings.py
+++ app/iotproject/settings.py
@@ -30,7 +30,7 @@ def is_true(val):
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 SECRET_KEY = os.environ.get("SECRET_KEY", "foo")
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 PROJECT_ID = os.environ.get('PROJECT_ID')
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
