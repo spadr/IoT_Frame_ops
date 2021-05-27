c4a2abd9db7bc0ec366953936edd3509055abe92
sho <69189153+spadr@users.noreply.github.com>
2021-05-28 08:30:22 +0900

    Delete models.py

--- app/iot/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class IotModel(models.Model):
-    token = models.CharField(max_length=100)
-    device = models.TextField()
-    time = models.CharField(max_length=50)
-    content = models.TextField()
