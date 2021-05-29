cbc5179419aa454341f29337038032bfd00c0ce2
spadr <ennbunn1626@yahoo.co.jp>
2021-05-29 15:53:25 +0900

    err_handring

--- app/iot/views/make_user.py
+++ app/iot/views/make_user.py
@@ -12,30 +12,37 @@ import secrets
 
 
 def signupfunc(request):
-    if request.method == "POST":
+    if request.method == "POST":#POSTの処理
         email = request.POST['emailadress']
         psw = request.POST['password']
 
+        #ユーザー登録
         try :
             user = User.objects.create_user(email, email, psw)
         except :
+            #ユーザー登録NG
             return render(request, 'signup.html', {'error' : 'このユーザーはすでに登録されています。'})
         
+        #Accesskeyの生成
         try:
             hash = getattr(settings, 'PROJECT_ID', ' ') + secrets.token_hex()
             user.is_active = False
             user.last_name = hash
             user.save()
         except :
+            #ダメなとき
             return render(request, 'signup.html', {'error' : 'DBへの入力値が不正です。'})
         
+        #認証メールの作成
         try:
             from_email = 'EMAIL_ADDRESS'
             recipient_list = [email]
-            subject = 'Activate Your Account'
+            subject = 'Activate Your Account'#メールタイトル
             current_site = get_current_site(request)
             domain = current_site.domain
-            context = render_to_string('account_activation_email.html',{
+            #内容はtemplateから
+            context = render_to_string('account_activation_email.html',
+            {
                 'protocol': request.scheme,
                 'domain': domain,
                 'token': dumps(user.pk),
@@ -43,36 +50,43 @@ def signupfunc(request):
                 'hash': hash,
             })
         except :
+            #ダメなとき
             return render(request, 'signup.html', {'error' : 'メール関係の変数が不正です。'})
         
+        #認証メールの作成
         try:
             send_mail(subject, context, from_email, recipient_list)
             return render(request, 'signup.html', {'error' : '登録したメールアドレスへ認証メールを送信しました。URLをクリックして、アカウントを有効化してください。' , 'error2':'Please confirm your email address to complete the registration'})
         except :
+            #ダメなとき
             return render(request, 'signup.html', {'error' : 'ユーザーの登録は完了しましたが、認証メールを送信に失敗しました。' , 'error2':'入力したメールアドレスを再度ご確認の上、管理者にお問い合わせください。'})
     
-    return render(request, 'signup.html')
+    return render(request, 'signup.html')#GETのレスポンス
 
 
 
 def completefunc(request, **kwargs):
-    timeout_seconds = getattr(settings, 'ACTIVATION_TIMEOUT_SECONDS', 60*60*24)
+    timeout_seconds = getattr(settings, 'ACTIVATION_TIMEOUT_SECONDS', 60*60*24)#認証メールの期限
     token = kwargs.get('token')
+
+    #認証メールの期限チェック
     try:
         user_pk = loads(token, max_age=timeout_seconds)
-        print('user_pk:',user_pk)
     except SignatureExpired:
+        #ダメなとき
         return HttpResponseBadRequest()
     except BadSignature:
+        #ダメなとき
         return HttpResponseBadRequest()
     else:
+        #認証メールの期限OK
+        
+        #ユーザーを有効化
         try:
             user = User.objects.get(pk=user_pk)
-            print('user:',user)
         except User.DoesNotExist:
             return HttpResponseBadRequest()
         else:
-            print('user.is_active:',user.is_active)
             user.is_active = True
             user.save()
             return redirect('meme')
--- app/iot/views/view_page.py
+++ app/iot/views/view_page.py
@@ -16,55 +16,69 @@ import datetime
 from django_pandas.io import read_frame
 
 
-def dict_to_list(dic):
-    lst = []
-    for e in dic.items():
-        lst.extend(e)
-    return lst
-
 
 def memefunc(request):
     host = settings.ALLOWED_HOSTS
-    port = 8025
+    port = 8025#docker-compose.ymlで設定したmailhog_urlのHTTPポート
+
     mailhog_url = 'http://' + host[0] + ':' + str(port) + '/'
+
     return render(request, 'top.html', {'mailhog_url': mailhog_url})
 
 
 @login_required
 def readfunc(request):
-    try:
-        username = request.user.get_username()
-        t =User.objects.filter(username__contains=username).values('last_name')
-        user_db = IotModel.objects.filter(token__contains=t)
-        df = read_frame(user_db.order_by('-time'), fieldnames=['device', 'time', 'content'])
-        dt = [datetime.datetime.fromtimestamp(int(i)) for i in df['time']]
-        df['time'] = dt
-        html_object = df.to_html(classes='table table-light table-striped table-hover table-bordered table-responsive')
-        return render(request, 'detail.html', {'table':html_object ,'username':username})
-    except:
-        return redirect('login')
+    #ユーザーが登録したデータを取得
+    username = request.user.get_username()
+    t =User.objects.filter(username__contains=username).values('last_name')
+    user_db = IotModel.objects.filter(token__contains=t)
+    df = read_frame(user_db.order_by('-time'), fieldnames=['device', 'time', 'content'])
+
+    #UNIX時間を普通の日時表示に変更
+    dt = [datetime.datetime.fromtimestamp(int(i)) for i in df['time']]
+    df['time'] = dt
+
+    html_object = df.to_html(classes='table table-light table-striped table-hover table-bordered table-responsive')
+
+    return render(request, 'detail.html', {'table':html_object ,'username':username})
 
 
 
 @login_required
 def graphfunc(request):
-    try:
-        username = request.user.get_username()
-        t =User.objects.filter(username__contains=username).values('last_name')
-        user_db = IotModel.objects.filter(token__contains=t)
-        df = read_frame(user_db.order_by('-time'), fieldnames=['device', 'time', 'content'])
-        device_name = df['device'] 
-        device_name_set = device_name[~device_name.duplicated()]
-        device_time = df['time']
-        device_content = df['content']
-        device_content_list = [device_content[device_name == i] for i in device_name_set]
-        device_time_list = [device_time[device_name == i] for i in device_name_set]
-        fig = go.Figure()
-        for c,j,n in zip(device_content_list , device_time_list , device_name_set):
-            data_y = [float(i.split(',')[0]) for i in np.array(c)]
-            data_x = [pd.to_datetime(int(i)+60*60*9, unit='s') for i in np.array(j).flatten()]
-            fig.add_trace(go.Scatter(x=data_x, y=data_y, mode='lines+markers',name=str(n)))
-        plot_fig = plot(fig, output_type='div', include_plotlyjs=False)
-        return render(request, 'graph.html', {'plot_gantt':plot_fig ,'username':username})
-    except:
-        return redirect('read')
+    #ユーザーが登録したデータを取得
+    username = request.user.get_username()
+    t =User.objects.filter(username__contains=username).values('last_name')
+    user_db = IotModel.objects.filter(token__contains=t)
+
+    #データの形を整える
+    df = read_frame(user_db.order_by('-time'), fieldnames=['device', 'time', 'content'])
+
+    device_name = df['device'] 
+    device_name_set = device_name.drop_duplicates()
+    device_time = df['time']
+    device_content = df['content']
+    device_content_list = [device_content[device_name == i] for i in device_name_set]
+    device_time_list = [device_time[device_name == i] for i in device_name_set]
+    
+    #グラフ描画
+    fig = go.Figure()
+    for c,j,n in zip(device_content_list , device_time_list , device_name_set):#デバイス毎にfor
+        data_y = []
+        data_x = []
+        for y,x in zip(np.array(c),np.array(j).flatten()):
+            #数値以外が登録されていた場合は無視
+            try:
+                num = float(y.split(',')[0])#最初の数字をグラフに表示
+                data_y.append(num)
+            except:
+                break
+            else:
+                time = pd.to_datetime(int(x)+60*60*9, unit='s')#日本時間UTC+9
+                data_x.append(time)
+        
+        fig.add_trace(go.Scatter(x=data_x, y=data_y, mode='lines+markers',name=str(n)))
+    
+    plot_fig = plot(fig, output_type='div', include_plotlyjs=False)
+
+    return render(request, 'graph.html', {'plot_gantt':plot_fig ,'username':username})
