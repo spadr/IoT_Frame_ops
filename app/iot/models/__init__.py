dbc93e730283e4a1c9e2e70ae432944f736d3fc1
spadr <ennbunn1626@yahoo.co.jp>
2022-04-23 08:25:07 +0000

    add ImageModel

--- app/iot/models/__init__.py
+++ app/iot/models/__init__.py
@@ -1,3 +1,3 @@
 from .custom_user import User
-from .element import BooleanModel, CharModel, FloatModel, IntModel
+from .element import BooleanModel, CharModel, FloatModel, ImageModel, IntModel
 from .tube import TubeModel
